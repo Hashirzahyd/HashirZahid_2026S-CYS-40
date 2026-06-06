609cb4fb5b881f3de8deae93899eeacaf25b1341
Hashirzahyd <itshashir227@gmail.com>
2026-06-06 17:21:13 +0500

    Add files via upload

---
+++ Week_4/Task3_Add_Two_Numbers_And_Return_Sum.py
@@ -0,0 +1,9 @@
+# LAB TASK 03: Define a function addNumbers(a, b) that returns the sum of two numbers.
+
+def addNumbers(a, b):
+    Total = a + b
+    return Total
+
+num1 = int(input("please enter number:"))
+num2 = int(input("please enter number:"))
+print(addNumbers(num1, num2))
