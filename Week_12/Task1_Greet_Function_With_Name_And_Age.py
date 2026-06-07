f065e7d0e7ceec3b4e1375cc428c67eb631aedb7
Hashirzahyd <itshashir227@gmail.com>
2026-06-07 15:45:21 +0500

    Delete Week_12 directory

--- Week_12/Task1_Greet_Function_With_Name_And_Age.py
+++
@@ -1,5 +0,0 @@
-def greet(name, age):
-    print(f"Welcome {name}!")
-    print(f"You are {age} years old.")
-
-greet("Hashir", 19)
--- Week_12/Task3_Division_By_Zero_Exception_Handling.py
+++
@@ -1,6 +0,0 @@
-num = int(input("Enter a number: "))
-try:
-    b = num / 0
-    print(f"The result of dividing {num} by zero is {b}")
-except Exception as e:
-    print(f"Error: {e}")
