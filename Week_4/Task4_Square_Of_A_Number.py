ed444624ca68a459dc0948b99fc9827f7a7e48b1
Hashirzahyd <itshashir227@gmail.com>
2026-06-07 15:44:58 +0500

    Delete Week_4 directory

--- Week_4/Task4_Square_Of_A_Number.py
+++
@@ -1,7 +0,0 @@
-# LAB TASK 04: Define a function square(num) that returns the square of the given number.
-
-def square(Number):
-    return (Number ** 2)
-
-Number = int(input("Enter a Number:"))
-print(square(Number))
