65017921892287b8fb19f292f8e0c99d8486bb06
Hashirzahyd <itshashir227@gmail.com>
2026-06-07 15:44:32 +0500

    Delete Week_9 directory

--- Week_9/Task3_Fruits_Dictionary_Scientific_Names.py
+++
@@ -1,9 +0,0 @@
-fruits = {
-    "Malus domestica": "Apple",
-    "Musa acuminata": "Banana",
-    "Vitis vinifera": "Grapes",
-    "Mangifera indica": "Mango"
-}
-print(fruits["Mangifera indica"])
-for k, v in fruits.items():
-    print(k, ":", v)
