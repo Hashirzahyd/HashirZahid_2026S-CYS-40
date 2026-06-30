4fb5239561d9131ec2759f1c3604f686d1e16e74
Hashirzahyd <itshashir227@gmail.com>
2026-06-30 20:06:21 +0500

    Delete Week2 directory

--- Week2/Task1_Binary_To_Decimal.py
+++
@@ -1,8 +0,0 @@
-# LAB TASK 01: Write a program that convert a binary number into its decimal equivalent.
-binary = input("Enter a binary number: ")
-decimal = 0
-power = 0
-for digit in reversed(binary):
-    decimal = decimal + int(digit) * (2 ** power)
-    power = power + 1
-print("Answer is:", decimal)
