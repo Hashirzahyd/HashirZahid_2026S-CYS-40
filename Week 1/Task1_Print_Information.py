cfffb4abb8c79fccd727881a2229f1d08f7ce634
Hashirzahyd <itshashir227@gmail.com>
2026-06-30 20:01:50 +0500

    Add files via upload

---
+++ Week 1/Task1_Print_Information.py
@@ -0,0 +1,4 @@
+#Task1: Print Your Information
+print("Name: Hashir Zahid")
+print("Department: Computer Engineering")
+print("University: UET Lahore")
---
+++ Week 1/Task2_Using_Sep_And_End.py
@@ -0,0 +1,4 @@
+#Task 2: Using sep and end
+print("UET","Lahore","Computer","Engineering", sep="-")
+print("UET","Lahore", end="-")
+print("Computer","Engineering")
