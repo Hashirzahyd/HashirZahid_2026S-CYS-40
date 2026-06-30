c5f65ea512b27374afad84ecba69a3a29055328d
Hashirzahyd <itshashir227@gmail.com>
2026-06-30 19:56:40 +0500

    Add files via upload

---
+++ Week6/Task 1.py
@@ -0,0 +1,5 @@
+#Task 1
+for i in range(10):
+    for j in range(i):
+        print("*", end=" ")
+    print()
---
+++ Week6/Task 2.py
@@ -0,0 +1,10 @@
+#Task 2
+for i in range(10):
+    for j in range(i):
+        print("~", end=" ")
+    print()
+
+for i in range(1, 2):
+    for j in range(10):
+        print(j*"*")
+    print()
