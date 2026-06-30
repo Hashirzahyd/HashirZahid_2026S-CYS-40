1bdf8d0b9da74601839d2aca26c26cb8b84bb087
Hashirzahyd <itshashir227@gmail.com>
2026-06-30 19:57:51 +0500

    Add files via upload

---
+++ Week13/Task 5.py
@@ -0,0 +1,11 @@
+#Task 5
+
+# a = open("data.txt","r")
+# s= a.read()
+# print(s)
+# a.close()
+
+a = open("write data.txt","a")
+s= a.write("is my name")
+print(s)
+a.close()
---
+++ Week13/Task4.py
@@ -0,0 +1,8 @@
+#Task 4
+x="45"
+y="    "
+def welcome():
+    x = 40
+    print(x)
+    print(globals()["x"])
+welcome()
