25b52d8f8112f136c62e7383eacf3e961a70418e
Hashirzahyd <itshashir227@gmail.com>
2026-06-30 19:17:30 +0500

    Add files via upload

---
+++ Week 13/Hashir.py
@@ -0,0 +1,9 @@
+#Task 3
+def welcome():
+    print("Welcome Message")
+
+def Goodbye():
+    print("Goodbye Message")
+if __name__=='__main__':
+    welcome()
+    Goodbye()
---
+++ Week 13/Task 6.py
@@ -0,0 +1,5 @@
+#Task 6
+a = open("write data.txt","a")
+s= a.write(" Roger that ")
+print(s)
+a.close()
